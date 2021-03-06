8cfa4c1ac8b238498854ffec655f25d103459477
Deathreus <csmoore96@yahoo.com>
2021-03-06 01:10:19 -0500

    Rework the linting delays and events

--- src/extension.ts
+++ src/extension.ts
@@ -1,83 +1,122 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import { workspace, window, languages, commands } from 'vscode';
 import * as vscode from 'vscode';
 import { glob } from 'glob';
 import * as path from 'path';
-import { execSync } from 'child_process';
+import { execFileSync } from 'child_process';
+import * as fs from 'fs';
 
-let timeout: NodeJS.Timeout | undefined = undefined;
+class DelayedFunc {
+	private timeout: NodeJS.Timeout | undefined;
 
-function refreshDiagnostics(document: vscode.TextDocument, context: vscode.DiagnosticCollection) {
-	if(timeout)
-	{
-		clearTimeout(timeout);
-		timeout = undefined;
+	constructor () {
+		this.timeout = undefined;
 	}
 
-	timeout = setTimeout(function() {
-		if(path.extname(document.fileName) === '.sp') {
-			let diagnostics: vscode.Diagnostic[] = [];
-			try {
-				execSync(__dirname + "/../spcomp.exe -i" + workspace.getConfiguration("sourcePawnLinter").get("includeDir") + " --dry-run " + document.uri.fsPath);
-			} catch (error) {
-				let regex = /\((\d+)+\) : ((error|fatal error|warning).+)/gm;
-				let matches: RegExpExecArray | null;
-				while(matches = regex.exec(error.stderr?.toString() ?? "")) {
-					let range = new vscode.Range(new vscode.Position(Number(matches[1]) - 1, 0), new vscode.Position(Number(matches[1]) - 1, 200));
-					let severity = matches[3] === 'warning' ? vscode.DiagnosticSeverity.Warning : vscode.DiagnosticSeverity.Error;
-					diagnostics.push(new vscode.Diagnostic(range, matches[2], severity));
-				}
-			}
-	
-			context.set(document.uri, diagnostics);
+	/**
+	 * Starts a time delayed execution of a function
+	 * @param callback: The function to execute
+	 * @param delay:	Delay before running, in ms
+	 */
+	public start(callback: (...args: any[]) => void, delay: number) {
+		this.timeout = setTimeout(callback, delay);
+	}
+
+	/**
+	 * Cancels the current delayed execution if one exists
+	 */
+	public cancel() {
+		if(this.timeout)
+		{
+			clearTimeout(this.timeout);
+			this.timeout = undefined;
 		}
-	}, 100);
+	}
 }
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+const tempFile = path.join(__dirname, "temp.sp");
+const spcomp = path.join(__dirname, "spcomp");
+
 export function activate(context: vscode.ExtensionContext) {
-	let compilerDiagnostics = languages.createDiagnosticCollection("compiler");
+	let throttles: { [Key: string]: DelayedFunc } = {};
+	function refreshDiagnostics(document: vscode.TextDocument, compilerDiagnostics: vscode.DiagnosticCollection) {
+		let throttle = throttles[document.uri.path];
+		if(throttle === undefined)
+		{
+			throttle = new DelayedFunc;
+			throttles[document.uri.path] = throttle;
+		}
+
+		throttle.cancel();
+		throttle.start(function() {
+			if(path.extname(document.fileName) === '.sp') {
+				let diagnostics: vscode.Diagnostic[] = [];
+				try {
+					
+					let file = fs.openSync(tempFile, "w");
+					fs.writeSync(file, document.getText());
+					fs.closeSync(file);
+
+					execFileSync(spcomp, [ "-i" + vscode.workspace.getConfiguration("sourcePawnLinter").get("includeDir") ,"--dry-run", tempFile ]);
+				} catch (error) {
+					let regex = /\((\d+)+\) : ((error|fatal error|warning).+)/gm;
+					let matches: RegExpExecArray | null;
+					while(matches = regex.exec(error.stderr?.toString() || "")) {
+						const range = new vscode.Range(new vscode.Position(Number(matches[1]) - 1, 0), new vscode.Position(Number(matches[1]) - 1, Infinity));
+						const severity = matches[3] === 'warning' ? vscode.DiagnosticSeverity.Warning : vscode.DiagnosticSeverity.Error;
+						diagnostics.push(new vscode.Diagnostic(range, matches[2], severity));
+					}
+				}
+		
+				compilerDiagnostics.set(document.uri, diagnostics);
+			}
+		}, 300);
+	}
+	vscode.window.showInformationMessage("Starting SP Linter");
+
+	let compilerDiagnostics = vscode.languages.createDiagnosticCollection("compiler");
 	context.subscriptions.push(compilerDiagnostics);
 
-	if(window.activeTextEditor) {
-		refreshDiagnostics(window.activeTextEditor.document, compilerDiagnostics);
-	}
-
-	let activeTextEditorChanged = window.onDidChangeActiveTextEditor(editor => { 
+	let activeEditorChanged = vscode.window.onDidChangeActiveTextEditor(editor => { 
 		if(editor) {
 			refreshDiagnostics(editor.document, compilerDiagnostics); 
 		}
 	});
-	context.subscriptions.push(activeTextEditorChanged);
+	context.subscriptions.push(activeEditorChanged);
 
-	let textDocumentChanged = workspace.onDidChangeTextDocument(event => {
+	let textDocumentChanged = vscode.workspace.onDidChangeTextDocument(event => {
 			refreshDiagnostics(event.document, compilerDiagnostics);
 	});
 	context.subscriptions.push(textDocumentChanged);
 
-	let textDocumentClosed = workspace.onDidCloseTextDocument(document => {
+	let textDocumentClosed = vscode.workspace.onDidCloseTextDocument(document => {
 		compilerDiagnostics.delete(document.uri);
+		delete throttles[document.uri.path];
 	});
 	context.subscriptions.push(textDocumentClosed);
 
-	glob(path.join(workspace.workspaceFolders?.[0].uri.path ?? "", "**/include/sourcemod.inc"), (err, files) => {
+	glob(path.join(vscode.workspace.workspaceFolders?.[0].uri.path || "", "**/include/sourcemod.inc"), (err, files) => {
         if (files.length === 0) {
-            if (!workspace.getConfiguration("sourcePawnLinter").get("includeDir")) {
-                window.showWarningMessage("SourceMod API not found in the project. You may need to set SourceMod Home for autocompletion to work", "Open Settings").then((choice) => {
+            if (!vscode.workspace.getConfiguration("sourcePawnLinter").get("includeDir")) {
+                vscode.window.showWarningMessage("SourceMod API not found in the project. You may need to set SourceMod Home for autocompletion to work", "Open Settings").then((choice) => {
                     if (choice === 'Open Settings') {
-                        commands.executeCommand("workbench.action.openWorkspaceSettings");
+                        vscode.commands.executeCommand("workbench.action.openWorkspaceSettings");
                     }
                 });
             }
         } else {
-            if (!workspace.getConfiguration("sourcepawnLinter").get("includeDir")) {
-                workspace.getConfiguration("sourcepawnLinter").update("includeDir", path.dirname(files[0]));
+            if (!vscode.workspace.getConfiguration("sourcepawnLinter").get("includeDir")) {
+                vscode.workspace.getConfiguration("sourcepawnLinter").update("includeDir", path.dirname(files[0]));
             }
         }
     });
+
+	fs.chownSync(spcomp, 0o777, 0o777);
+	vscode.workspace.textDocuments.forEach(document => { refreshDiagnostics(document, compilerDiagnostics); });
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+	try {
+		fs.unlinkSync(tempFile);
+	} catch {
+
+	}
+}
